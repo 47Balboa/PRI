c57b107447163d8f41b053106feca9d315cf7975
Shahzod Yusupov <36776778+47Balboa@users.noreply.github.com>
2019-11-11 12:57:56 +0000

    Add files via upload
    TP6

---
+++ TP6/filmes/controllers/filmes.js
@@ -0,0 +1,42 @@
+var Filme = require('../models/filmes')
+var ObjectId = require('mongodb').ObjectID
+
+// Devolve a lista de filmes
+module.exports.listar = () => {
+    return Filme
+        .find()
+        .exec()
+}
+
+/*
+module.exports.consultar = id => {
+    return Filme
+        .findOne(ObjectId(id))
+        .exec()
+}
+
+*/
+module.exports.consultar = ident => {
+    return Filme
+            .findOne({_id:ident})
+            .exec()
+}
+
+module.exports.contar = () => {
+    return Filme
+        .countDocuments()
+        .exec()
+}
+
+module.exports.inserir = filme => {
+    var novo = new Filme(filme)
+    return novo.save()
+               
+}
+
+module.exports.remover = id =>{
+    return Filme
+        .findByIdAndRemove({_id:id})
+        .exec()
+}
+
